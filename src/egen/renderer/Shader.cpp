4d8e03f202ad270e8b1c76aacf5ceb15ce0304ae
dma-neves <david.m.neves@live.com>
2026-01-10 13:34:20 +0000

    compile shader refactor

--- src/egen/renderer/Shader.cpp
+++ src/egen/renderer/Shader.cpp
@@ -22,98 +22,38 @@ static std::string load_file(const std::filesystem::path& path)
 
 Shader::Shader(const std::filesystem::path& common_shader_path, const std::filesystem::path& vertex_shader_path, const std::filesystem::path& fragment_shader_path)
 {
-    std::string common_vertex_shader_source = load_file(common_shader_path/"common.vert");
-    std::string common_fragment_shader_source = load_file(common_shader_path/"common.frag");
-    std::string common_light_fragment_shader_source = load_file(common_shader_path/"light.frag");
-    std::string vertex_shader_source = load_file(vertex_shader_path);
-    std::string fragment_shader_source = load_file(fragment_shader_path);
 
-    const char* common_vertex_shader_source_c_str = common_vertex_shader_source.c_str();
-    const char* common_fragment_shader_source_c_str = common_fragment_shader_source.c_str();
-    const char* common_light_fragment_shader_source_c_str = common_light_fragment_shader_source.c_str();
-    const char* vertex_shader_source_c_str = vertex_shader_source.c_str();
-    const char* fragment_shader_source_c_str = fragment_shader_source.c_str();
+    std::filesystem::path common_vertex_shader_path = common_shader_path/"common.vert";
+    std::filesystem::path common_fragment_shader_path = common_shader_path/"common.frag";
+    std::filesystem::path common_light_fragment_shader_path = common_shader_path/"light.frag";
+
+    create_program(
+        compile_shader(GL_VERTEX_SHADER, common_vertex_shader_path),
+        compile_shader(GL_VERTEX_SHADER, vertex_shader_path),
+        compile_shader(GL_FRAGMENT_SHADER, common_fragment_shader_path),
+        compile_shader(GL_FRAGMENT_SHADER, common_light_fragment_shader_path),
+        compile_shader(GL_FRAGMENT_SHADER, fragment_shader_path)
+    );
+}
+
+GLuint Shader::compile_shader(GLenum shader_type, const std::filesystem::path& shader_path)
+{
+    std::string shader_source = load_file(shader_path);
+    const char* shader_source_c_str = shader_source.c_str();
+    GLuint shader_id = glCreateShader(shader_type);
+    glShaderSource(shader_id, 1, &shader_source_c_str, NULL);
 
     int success;
     char info_log[512];
-
-    // Compile Common Vertex Shader
-    GLuint common_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(common_vertex_shader, 1, &common_vertex_shader_source_c_str, NULL);
-    glCompileShader(common_vertex_shader);
-    glGetShaderiv(common_vertex_shader, GL_COMPILE_STATUS, &success);
+    glCompileShader(shader_id);
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
     if (!success)
     {
-        glGetShaderInfoLog(common_vertex_shader, 512, NULL, info_log);
-        throw std::runtime_error(std::string("Failed to compile common vertex shader [") + (common_shader_path/"common.vert").string() + "]" + info_log);
+        glGetShaderInfoLog(shader_id, 512, NULL, info_log);
+        throw std::runtime_error(std::string("Failed to compile ") + shader_path.string() + ": " + info_log);
     }
 
-    // Compile Vertex Shader
-    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_source_c_str, NULL);
-    glCompileShader(vertex_shader);
-    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
-        throw std::runtime_error(std::string("Failed to compile vertex shader [") + std::string(vertex_shader_path) + std::string("]") + info_log);
-    }
-
-    // Compile Common Fragment Shader
-    GLuint common_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(common_fragment_shader, 1, &common_fragment_shader_source_c_str, NULL);
-    glCompileShader(common_fragment_shader);
-    glGetShaderiv(common_fragment_shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(common_fragment_shader, 512, NULL, info_log);
-        throw std::runtime_error(std::string("Failed to compile common fragment shader [") + (common_shader_path/"common.frag").string() + "]" + info_log);
-    }
-
-    // Compile Common Light Fragment Shader
-    GLuint common_light_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(common_light_fragment_shader, 1, &common_light_fragment_shader_source_c_str, NULL);
-    glCompileShader(common_light_fragment_shader);
-    glGetShaderiv(common_light_fragment_shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(common_light_fragment_shader, 512, NULL, info_log);
-        throw std::runtime_error(std::string("Failed to compile common light fragment shader [") + (common_shader_path/"light.frag").string() + "]" + info_log);
-    }
-
-    // Compile Fragment Shader
-    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_source_c_str, NULL);
-    glCompileShader(fragment_shader);
-    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
-        throw std::runtime_error(std::string("Failed to compile fragment shader [") + std::string(fragment_shader_path) + std::string("]") + info_log);
-    }
-
-    // Create Shader Program, attach previous shaders, and link the shaders
-    m_shader_program = glCreateProgram();
-    glAttachShader(m_shader_program, common_vertex_shader);
-    glAttachShader(m_shader_program, vertex_shader);
-    glAttachShader(m_shader_program, common_fragment_shader);
-    glAttachShader(m_shader_program, common_light_fragment_shader);
-    glAttachShader(m_shader_program, fragment_shader);
-    glLinkProgram(m_shader_program);
-    glGetProgramiv(m_shader_program, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(m_shader_program, 512, NULL, info_log);
-        throw std::runtime_error(std::string("Failed to link shader program: ") + info_log);
-    }
-    // Activate the Shader Program
-    glUseProgram(m_shader_program);
-    // After the shaders have been linked to the program, we can delete them
-    glDeleteShader(common_vertex_shader);
-    glDeleteShader(vertex_shader);
-    glDeleteShader(common_fragment_shader);
-    glDeleteShader(common_light_fragment_shader);
-    glDeleteShader(fragment_shader);
+    return shader_id;
 }
 
 GLuint Shader::program()
--- src/egen/renderer/Shader.hpp
+++ src/egen/renderer/Shader.hpp
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <cstdint>
 #include <unordered_map>
+#include <vector>
 
 #include "platform/glad/include/glad/glad.h"
 #include <GLFW/glfw3.h>
@@ -33,6 +34,31 @@ public:
     }
 
 private:
+    GLuint compile_shader(GLenum shader_type, const std::filesystem::path& shader_path);
+    void create_program(std::vector<GLuint>&& shaders);
+
+    template <typename... ShaderIds>
+    void create_program(ShaderIds... shaders)
+    {
+        int success;
+        char info_log[512];
+        m_shader_program = glCreateProgram();
+
+        (glAttachShader(m_shader_program, shaders), ...);
+
+        glLinkProgram(m_shader_program);
+        glGetProgramiv(m_shader_program, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(m_shader_program, 512, NULL, info_log);
+            throw std::runtime_error(std::string("Failed to link shader program: ") + info_log);
+        }
+        // Activate the Shader Program
+        glUseProgram(m_shader_program);
+        // After the shaders have been linked to the program, we can delete them
+        (glDeleteShader(shaders), ...);
+    }
+
     GLuint m_shader_program;
     std::unordered_map<std::string, GLint> m_uniform_cache;
     GLint get_uniform_location(const std::string& name);
